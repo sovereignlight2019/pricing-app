124a3ba57835de729867d90bc9baf206af9e1ba8
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 17:16:45 +0100

    saddsfdfs

--- pricing/pricing/config.py
+++ pricing/pricing/config.py
@@ -5,6 +5,29 @@ from dotenv import load_dotenv
 basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, ".env"))
 
+class Config:
+    """Configuration from environment variables."""
+
+    #SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_ENV = environ.get("FLASK_ENV")
+    FLASK_APP = "wsgi.py"
+
+    # Flask-Assets
+    #LESS_BIN = environ.get("LESS_BIN")
+    #ASSETS_DEBUG = True
+    #LESS_RUN_IN_DEBUG = True
+
+    # Static Assets
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    COMPRESSOR_DEBUG = True
+
+    # Datadog
+    # DD_SERVICE = environ.get("DD_SERVICE")
+
+    # API
+    # BEST_BUY_API_KEY = environ.get("BEST_BUY_API_KEY")
+
 def connect():
     """ Connect to the PostgreSQL database server """
     conn = None
--- pricing/pricing/pricing/__init__.py
+++ pricing/pricing/pricing/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from os import environ, path
-from dotenv import load_dotenv
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, ".env"))
 
 def init_app():
     """Create Flask application."""
