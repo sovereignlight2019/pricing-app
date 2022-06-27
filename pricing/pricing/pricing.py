3be0f73ee1e02c3859145ae9b611995f174d714b
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 15:28:40 +0100

    removed config

--- pricing/pricing/pricing.py
+++ pricing/pricing/pricing.py
@@ -3,7 +3,6 @@ from flask import Flask
 def create_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
 
     with app.app_context():
         # Import parts of our application
