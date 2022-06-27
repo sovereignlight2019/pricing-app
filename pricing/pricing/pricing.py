a991dc9d6cfc4fa0e59f4e0d17abfaa3279a598f
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 16:01:42 +0100

    piss off

--- pricing/pricing/pricing.py
+++ pricing/pricing/pricing.py
@@ -2,6 +2,9 @@ from flask import Flask
 from os import environ, path
 from dotenv import load_dotenv
 
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
 def create_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
