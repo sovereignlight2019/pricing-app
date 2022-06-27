d7a6f056e00ad6a2d77af1a3823593b43952b5b4
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 17:30:34 +0100

    sdfdsffds

--- pricing/pricing/pricing/__init__.py
+++ pricing/pricing/pricing/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from os import environ, path
+from config import Config
 
 def init_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object("config.Config")
 
     with app.app_context():
         # Import parts of our application
-        from .home import routes
+        from .home import home
 
         # Register Blueprints
         app.register_blueprint(home.home_bp)
