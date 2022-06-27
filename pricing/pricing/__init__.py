04a8571b7283e0294770319fd06e02a915032a7a
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 16:13:29 +0100

    asd

---
+++ pricing/pricing/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from config import Config
+
+def init_app():
+    """Create Flask application."""
+    app = Flask(__name__, instance_relative_config=False)
+
+    with app.app_context():
+        # Import parts of our application
+        from .home import routes
+
+        # Register Blueprints
+        app.register_blueprint(home.home_bp)
+
+        return app
