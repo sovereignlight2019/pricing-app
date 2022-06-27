a3ff6d530c5ba6c5bd9088cf345604496337a0f7
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 15:50:44 +0100

    add confi

--- pricing/pricing/config.py
+++ pricing/pricing/config.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 import psycopg2
 from config import config
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
 
 def connect():
     """ Connect to the PostgreSQL database server """
