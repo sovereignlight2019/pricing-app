dbc0d5feef9d6e91b05dc343ba1be5f2ffe4ac41
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 23:22:24 +0100

    sdfsdf

--- pricing/pricing/pricing/__init__.py
+++ pricing/pricing/pricing/__init__.py
@@ -5,12 +5,15 @@ def init_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object("config.Config")
+    assets = Environment()  # Create an assets environment
+    assets.init_app(app)  # Initialize Flask-Assets
 
     with app.app_context():
         # Import parts of our application
         from .home import home
+        from .assets import compile_static_assets
 
         # Register Blueprints
         app.register_blueprint(home.home_bp)
-
+        
         return app
