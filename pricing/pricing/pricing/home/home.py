54e8da1a2e728589d8b19f268d0cb54b355986c1
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-27 17:46:57 +0100

    sqdsqd

--- pricing/pricing/pricing/home/home.py
+++ pricing/pricing/pricing/home/home.py
@@ -9,11 +9,7 @@ home_bp = Blueprint(
     static_folder='static'
 )
 
-def generateMetrics():
-    return "hello world"
-
 @home_bp.route('/', methods=['GET'])
 def home():
-    response = make_response(generateMetrics(), 200)
     response.mimetype = "text/plain"
-    return response
+    return "hello world"
