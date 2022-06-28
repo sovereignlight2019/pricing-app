511f2d1c92901ad08a704f1d97e83384a191fd2f
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-06-28 10:00:33 +0100

    qw

--- pricing/pricing/pricing/register/register.py
+++ pricing/pricing/pricing/register/register.py
@@ -3,7 +3,7 @@ from flask import current_app as app
 
 
 # Blueprint Configuration
-home_bp = Blueprint(
+register_bp = Blueprint(
     'register_bp', __name__,
     template_folder='templates',
     static_folder='static'
