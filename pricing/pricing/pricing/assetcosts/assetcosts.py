c04602c9ec2a74ea53253e87dd1b7a3b73a3cfba
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-07-05 15:50:30 +0100

    asd

--- pricing/pricing/pricing/assetcosts/assetcosts.py
+++ pricing/pricing/pricing/assetcosts/assetcosts.py
@@ -13,18 +13,23 @@ assetcosts_bp = Blueprint(
 @assetcosts_bp.route('/assets', methods=['GET', 'POST'])
 def assets():
     if request.method == 'GET':
+        monthly_cost = []
         dbName = "sprocket"
         db = "dbname=" + dbName + " user=sprocket password=Sprocket123 host=localhost"
         conn = psycopg2.connect(db)
         cur = conn.cursor()
         cur.execute("""SELECT * FROM running_costs;""")
         rows = cur.fetchall()
+        cur.execute("""SELECT SUM (cost) AS total FROM running_costs;""")
+        cost = cur.fetchall()
+        for row in cost:
+            monthly_cost.append(row[0])
         cur.close()
         conn.close()
         my_list = []
         for row in rows:
             my_list.append(row)
-    return render_template("assetscosts.html", results=my_list)
+    return render_template("assetscosts.html", results=my_list, cost=monthly_cost)
 
 @assetcosts_bp.route('/assets/addcost', methods=['GET', 'POST'])
 def add_asset():
