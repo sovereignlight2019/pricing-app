f0cd84da75250ace668832b7db185af83c80cb3b
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-07-07 16:32:19 +0100

    s

--- pricing/pricing/pricing/media/media.py
+++ pricing/pricing/pricing/media/media.py
@@ -65,6 +65,47 @@ def vinyl_add():
 
         return redirect("/media/vinyl")
 
+@media_bp.route('/media/vinyl/edit/<id>', methods=['GET', 'POST'])
+def vinyl_edit(id):
+
+    db = "dbname=sprocket user=sprocket password=Sprocket123 host=localhost"
+    conn = psycopg2.connect(db)
+    cur = conn.cursor()
+
+    if request.method == 'GET':
+        cur.execute("""SELECT * FROM media WHERE id = %s;""", (id))
+        row = cur.fetchone()
+        cur.close
+        conn.close()
+    
+        return render_template("editvinyl.html", item=row)
+
+    else:
+        # Get Form data
+        vendor= request.form['inputVendor']
+        product = request.form['inputproduct']
+        supplier = request.form['inputSupplier']
+        rollwidth = request.form['inputRollWidth']
+        rolllength = request.form['inputRollLength']
+        cost = request.form['inputCost']
+        type = "vinyl"
+
+        # Update table row
+        conn = psycopg2.connect(db)
+        cur = conn.cursor()
+        cur.execute(
+           "UPDATE running_costs SET vendor=%s, product=%s, supplier=%s, roll_width=%s, roll_length, cost=%s"        
+           " WHERE id=%s",
+           (vendor,product,supplier,rollwidth,rolllength,cost,int(id),));
+        updated_rows = cur.rowcount
+        conn.commit()
+        cur.close
+        conn.close()
+
+        return redirect("/media/vinyl")
+
+
+
 @media_bp.route('/media/paper', methods=['GET'])
 def paper():
     return render_template("paper.html")
