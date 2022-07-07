57ecd9ce69316776b38a99a0c7484bfdad43a6ff
sovereignlight2019 <scottryan@sovereignlight.solutions>
2022-07-07 07:09:22 +0100

    added media

--- pricing/pricing/pricing/__init__.py
+++ pricing/pricing/pricing/__init__.py
@@ -1,3 +1,4 @@
+import imp
 from flask import Flask
 from config import Config
 
@@ -12,11 +13,13 @@ def init_app():
         from .register import register
         from .assetcosts import assetcosts
         from .calculator import calculator
+        from .media import media
 
         # Register Blueprints
         app.register_blueprint(home.home_bp)
         app.register_blueprint(register.register_bp)
         app.register_blueprint(assetcosts.assetcosts_bp)
         app.register_blueprint(calculator.calculator_bp)
+        app.register_blueprint(media.media_bp)
         
         return app
